8d76c775b63b6d27412be6897eee7e7397572f98
LenatDeririnc <smillerlenatderirinc@gmail.com>
2020-10-22 19:34:11 +0300

    Исправил проблему с Null-Reference ввода

---
+++ Assets/Scripts/InputActionSetup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputActionSetup
+{
+    public static MyInputActions MIA;
+
+    public static void Init()
+    {
+        MIA = new MyInputActions();
+        
+        MIA.Enable();
+    }
+
+}
