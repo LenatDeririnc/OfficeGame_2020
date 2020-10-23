02edb63f4f90a067a839890fdd05358e82e2777f
LenatDeririnc <smillerlenatderirinc@gmail.com>
2020-10-23 19:42:46 +0300

    Добавил систему включения терминала

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onCommandModeActivated;
+    public void CommandModeActivated()
+    {
+        if (onCommandModeActivated != null)
+        {
+            onCommandModeActivated();
+        }
+    }
+    
+    public event Action onCommandModeExited;
+    public void CommandModeExited()
+    {
+        if (onCommandModeExited != null)
+        {
+            onCommandModeExited();
+        }
+    }
+}
